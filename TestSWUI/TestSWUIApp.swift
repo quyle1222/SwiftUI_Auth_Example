8409863607d1388d777f5dec539ae10ba0d2b2ab
sinn <sinn@rikkeisoft.com>
2022-10-05 14:47:57 +0700

    Initial Commit

---
+++ TestSWUI/TestSWUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSWUIApp.swift
+//  TestSWUI
+//
+//  Created by TuPT on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestSWUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
